c6a365050d59461faf8d6e0bcfd398d8a78ac129
Eddie Leung <eddie.leung@foxtel.com.au>
2023-05-31 13:12:52 +1000

    Added parameterized tests

--- src/test/integration/org/learning/coursecatalogservice/repository/CourseRepositoryIntegrationTest.kt
+++ src/test/integration/org/learning/coursecatalogservice/repository/CourseRepositoryIntegrationTest.kt
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.learning.coursecatalogservice.utils.courses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("integration-test")
@@ -46,11 +50,41 @@ class CourseRepositoryIntegrationTest {
         courses.forEach { assertEquals(category, it.category) }
     }
 
+    @ParameterizedTest
+    //    @MethodSource("categoryAndExpectedSizeAsList")
+    @MethodSource("categoryAndExpectedSizeAsStream")
+    fun `should return courses with matching category`(category: String, expectedSize: Int) {
+        val courses = courseRepository.findByCategory(category)
+
+        assertEquals(expectedSize, courses.size)
+        courses.forEach { assertEquals(category, it.category) }
+    }
+
     @Test
     fun `should return empty list when no course with matching category`() {
         val category = "No matching category"
         val courses = courseRepository.findByCategory(category)
 
         assertEquals(0, courses.size)
+    }
+
+    companion object {
+        @JvmStatic
+        fun categoryAndExpectedSizeAsList() = listOf(
+            arrayOf("Programming", 2),
+            arrayOf("Framework", 1),
+            arrayOf("No matching category", 0),
+            arrayOf("programming", 0),
+            arrayOf("framework", 0)
+        )
+
+        @JvmStatic
+        fun categoryAndExpectedSizeAsStream() = Stream.of(
+            arguments("Programming", 2),
+            arguments("Framework", 1),
+            arguments("No matching category", 0),
+            arguments("programming", 0),
+            arguments("framework", 0)
+        )
     }
 }
